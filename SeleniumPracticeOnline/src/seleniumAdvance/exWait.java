d6ee37af3da280321ac6d9b507abc8719921e0bc
VetrivelM6688 <vetrivelmunusamy@gmail.com>
2022-02-08 16:46:00 +0530

    Wait pending operations

--- SeleniumPracticeOnline/src/seleniumAdvance/exWait.java
+++ SeleniumPracticeOnline/src/seleniumAdvance/exWait.java
@@ -14,10 +14,18 @@ public class exWait {
 
 	public static void main(String[] args) {
 
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Welcome\\Libs\\chrome\\chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
+		exWait ex = new exWait();
+		ex.disappearText();
+		//ex.appearText();
+		ex.waitForAlert();
+
+	}
+	WebDriver driver;
+	public void disappearText() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Welcome\\Libs\\drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
 		driver.get("http://www.leafground.com/pages/disapper.html");
-		
+
 		WebDriverWait hold = new WebDriverWait(driver, Duration.ofSeconds(20));
 		hold.until(ExpectedConditions.invisibilityOfElementWithText(By.id("btn"),
 				"I'm going to disappear. Keep looking at me!!"));
@@ -26,6 +34,31 @@ public class exWait {
 		System.out.println(postText.getText());
 
 		driver.quit();
+	}
+	public void appearText() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Welcome\\Libs\\drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/appear.html");
+
+		WebElement present = driver.findElement(By.id("btn"));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.textToBePresentInElement(present, "Voila! I'm here Guys"));
+		
+		System.out.println(present.getText());
+		driver.quit();
+	}
+	public void waitForAlert() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Welcome\\Libs\\drivers\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/alertappear.html");
+		WebElement clickBtn = driver.findElement(By.id("alert"));
+		clickBtn.click();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(8));
+		wait.until(ExpectedConditions.alertIsPresent());
+		
+		Alert al = driver.switchTo().alert();
+		al.accept();
 	}
 
 }
